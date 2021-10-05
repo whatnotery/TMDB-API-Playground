e7fd08551efcf6c5486c0ba43edd03849e051b0b
John O'shea <johncyoshea@me.com>
2021-10-05 17:21:34 -0400

    testing

--- app.js
+++ app.js
@@ -2,20 +2,32 @@
 const title = document.querySelector('#title')
 const poster = document.querySelector('#poster')
 const tagline = document.querySelector('#tagline')
+const id = document.querySelector('#id')
 
-
-fetch('https://api.themoviedb.org/3/movie/26914?api_key=b07d3efad9e75e49c88e831539462c48')
-.then(res => {
-return res.json()
-})
-.then(data => {
-    console.log(data)
-    title.textContent = `${data.title}`
-    poster.src = `https://image.tmdb.org/t/p/original/${data.poster_path}` 
-    tagline.textContent = `${data.tagline}`
-})
-.catch(err => {
+fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        return data.id
+    })
+    .then(data => {
+        let movieID = Math.floor(Math.random() * `${data.id}`);
+        fetch('https://api.themoviedb.org/3/movie/`${movieID}`?api_key=b07d3efad9e75e49c88e831539462c48')
+            .then(res => {
+                return res.json()
+            })
+            .then(data =>{
+                title.textContent = `${data.title}`
+                poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`
+                tagline.textContent = `${data.tagline}`
+            } )
+    })
+.catch (err => {
     console.log(err)
-})
+});
+
+
 
 

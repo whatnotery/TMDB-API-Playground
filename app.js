9e8f5d423671676ca8271337962873fbfb96f903
Jos O'shea <jos.oshea.ky@gmail.com>
2021-10-06 19:10:19 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const title = document.querySelector('#title')
 const poster = document.querySelector('#poster')
-const tagline = document.querySelector('#tagline')
+const plot = document.querySelector('#plot')
 const id = document.querySelector('#id')
 
 fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48')
@@ -13,22 +13,27 @@ fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831
         return parseInt(data.id)
     })
     .then(data => {
-        let movieID = Math.floor(Math.random() * data);
-        fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`)
-            .then(res => {
-                return res.json()
-            })
-            .then(data =>{
-                title.textContent = `${data.title}`
-                poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`
-                tagline.innerHTML = `"${data.tagline}" <br> ${data.overview}`
-            } )
+        getMovie(data)
     })
-.catch (err => {
-    console.log(err)
-});
+    .catch(err => {
+        console.log(err)
+    });
 
-// turn main logic into function, 
+function getMovie(data) {
+    let movieID = Math.floor(Math.random() * data);
+    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            title.textContent = `${data.title}`
+            poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`
+            plot.innerHTML = `${data.overview}`
+        })
+
+}
+
+ 
 //write if else logic if undefined or "data.adult = true" run function to display another film
 
 

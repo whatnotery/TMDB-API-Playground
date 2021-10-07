92b017f834b4800a9ecef0339cf3abf18ee91363
Jos O'shea <jos.oshea.ky@gmail.com>
2021-10-06 20:35:50 -0400

    added filter

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 const title = document.querySelector('#title')
 const poster = document.querySelector('#poster')
 const plot = document.querySelector('#plot')
-const id = document.querySelector('#id')
+let dataID = 0
 
 fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48')
     .then(res => {
         return res.json()
     })
     .then(data => {
-        console.log(data)
-        return parseInt(data.id)
+        
+         return parseInt(data.id)
     })
-    .then(data => {
+   .then(data => {
         getMovie(data)
     })
     .catch(err => {
@@ -26,14 +26,19 @@ function getMovie(data) {
             return res.json()
         })
         .then(data => {
-            title.textContent = `${data.title}`
-            poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`
-            plot.innerHTML = `${data.overview}`
+            if (data.adult === true || !data.title || !data.poster_path || !data.overview) {
+                console.log(data)
+                getMovie(movieID)
+            } else {
+                title.textContent = `${data.title}`
+                poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`
+                plot.innerHTML = `${data.overview}`
+            }
         })
 
 }
 
- 
+
 //write if else logic if undefined or "data.adult = true" run function to display another film
 
 

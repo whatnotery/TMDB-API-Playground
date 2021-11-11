a03438ef5c4adca4a1b6f95afcbc385bee42981e
Jos O'shea <jos.oshea.ky@gmail.com>
2021-11-10 23:18:07 -0500

    Added CSV export feature

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const imdblink = document.getElementById('imdbLink');
 const moviedblink = document.getElementById('theMovieDbLink');
 const newFilmButton = document.getElementById('newFilm');
 const saveFilmButton = document.getElementById('saveFilm')
+const exportButton = document.getElementById('exportFilm')
 let latestFilm
 let currentFilmData 
 let savedFilms = []
+
 fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48')
     .then(res => {
         return res.json()
@@ -25,6 +27,7 @@ fetch('https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831
         console.log(err)
     });
 
+    
 function getMovie(lastMovieID) {
     let movieID = Math.floor(Math.random() * lastMovieID);
     fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`)
@@ -46,14 +49,28 @@ function getMovie(lastMovieID) {
                 currentFilmData = data
             }
         })
-     
+    }
 
-}
+    function downloadCSV() {
+    ids = savedFilms.map( function (film) {
+    return film.id
+    })
+
+    let csvContent = "tmdbID, \n" + ids.join(", \n")
+
+    let csvData = new Blob([csvContent], { type: 'text/csv' });  
+    let csvUrl = URL.createObjectURL(csvData);
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = csvUrl;
+    hiddenElement.target = '_blank';
+    hiddenElement.download = "SavedFilms" + '.csv';
+    hiddenElement.click();
+            }
+
 
 
 newFilmButton.addEventListener('click' , function (e) { getMovie(latestFilm); })
 
 saveFilmButton.addEventListener('click' , function (e) {savedFilms.unshift(currentFilmData)} )
 
-
-
+exportButton.addEventListener('click' , function (e) { downloadCSV()})

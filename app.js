3120ae3a240d0f755c3a750bf6e801da08aa7e20
Jos O'shea <jos@joshea.dev>
2022-09-11 22:14:27 -0400

    test

--- app.js
+++ app.js
@@ -17,28 +17,29 @@ let latestFilm;
 let currentFilmData ;
 let savedFilms = [];
 
-fetch(`https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48`)
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        latestFilm = data.id;
-        return parseInt(data.id);
-    })
-   .then(data => {
-        getMovie(data);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-    
-function getMovie(lastMovieID) {
-    let movieID = Math.floor(Math.random() * lastMovieID);
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`)
-        .then(res => {
-            return res.json();
-        })
+//fetch(`https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48`)
+//    .then(res => {
+//        return res.json();
+//    })
+//    .then(data => {
+//        latestFilm = data.id;
+//        return parseInt(data.id);
+//    })
+//   .then(data => {
+//        getMovie(data);
+//    })
+//    .catch(err => {
+//        console.log(err);
+//    });
+//
+ //   
+function getMovie() {
+   // let movieID = Math.floor(Math.random() * lastMovieID);
+  // fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`
+    fetch(`https://randommovierailsapi-production.up.railway.app/films`)
+        //.then(res => {
+        //    return res.json();
+        //})
         .then(data => {
             if (data.adult === true || !data.title || !data.poster_path || !data.overview) {
                 getMovie(movieID);

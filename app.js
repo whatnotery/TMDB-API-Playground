186a9f59948469b47aa0cb29e97eab0d438f0571
Jos O'shea <jos@joshea.dev>
2022-09-12 01:11:27 -0400

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const toggleButton2 = document.getElementById('toggle2');
 const exportButton = document.getElementById('exportFilm');
 const filmContainer = document.getElementById('filmContainer')
 let latestFilm;
-let currentFilmData ;
+let currentFilmData;
 let savedFilms = [];
 
 //fetch(`https://api.themoviedb.org/3/movie/latest?api_key=b07d3efad9e75e49c88e831539462c48`)
@@ -32,50 +32,52 @@ let savedFilms = [];
 //        console.log(err);
 //    });
 //
- //   
+//   
 function getMovie() {
-   // let movieID = Math.floor(Math.random() * lastMovieID);
-  // fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`
+    // let movieID = Math.floor(Math.random() * lastMovieID);
+    // fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=b07d3efad9e75e49c88e831539462c48`
     fetch(`https://randommovierailsapi-production.up.railway.app/films`, {
         method: 'GET',
         headers: {
-          'X-Requested-With': 'XMLHttpRequest'
+            'X-Requested-With': 'XMLHttpRequest'
         },
-      })
+    })
         //.then(res => {
         //    return res.json();
         //})
         .then(data => {
-            if (data.adult === true || !data.title || !data.poster_path || !data.overview) {
-                getMovie(movieID);
-            } else {
-                title.textContent = `${data.title} (${data.release_date.slice(0,4)})`;
-                filmSearch.setAttribute('href',`https://www.google.com/search?q=${title.textContent.split(' ').join('+')}+film&tbm=vid`);
-                imdblink.setAttribute('href',`https://www.imdb.com/title/${data.imdb_id}`);
-                moviedblink.setAttribute('href',`https://www.themoviedb.org/movie/${data.id}`);
-                poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`;
-                poster.setAttribute('alt', `movie poster for ${title.textContent}`);
-                plot.innerHTML = `${data.overview}`;
-                currentFilmData = data;
-            };
+            //  if (data.adult === true || !data.title || !data.poster_path || !data.overview) {
+            //  getMovie(movieID);
+            //} else {
+            title.textContent = `${data.title} (${data.release_date.slice(0, 4)})`;
+            filmSearch.setAttribute('href', `https://www.google.com/search?q=${title.textContent.split(' ').join('+')}+film&tbm=vid`);
+            imdblink.setAttribute('href', `https://www.imdb.com/title/${data.imdb_id}`);
+            moviedblink.setAttribute('href', `https://www.themoviedb.org/movie/${data.id}`);
+            poster.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`;
+            poster.setAttribute('alt', `movie poster for ${title.textContent}`);
+            plot.innerHTML = `${data.overview}`;
+            currentFilmData = data;
+
         });
-    };
+};
+
+getMovie()
 
 function downloadCSV() {
-    ids = savedFilms.map( function (film) {
+    ids = savedFilms.map(function (film) {
         return film.id;
     });
 
     let csvContent = "tmdbID, \n" + ids.join(", \n");
 
-    let csvData = new Blob([csvContent], { type: 'text/csv' });  
+    let csvData = new Blob([csvContent], { type: 'text/csv' });
     let csvUrl = URL.createObjectURL(csvData);
     let hiddenElement = document.createElement('a');
     hiddenElement.href = csvUrl;
     hiddenElement.target = '_blank';
     hiddenElement.download = "SavedFilms" + '.csv';
     hiddenElement.click();
-            };
+};
 
 function switchContent() {
     if (libraryWrap.classList.contains('hide')) {
@@ -85,7 +87,7 @@ function switchContent() {
             noFilmsMessage = document.createElement('h2');
             noFilmsMessage.textContent = 'No Saved Films';
             libraryWrap.appendChild(noFilmsMessage);
-        } else {showLibrary()}
+        } else { showLibrary() }
     } else if (filmWrap.classList.contains('hide')) {
         libraryWrap.classList.add('hide');
         filmWrap.classList.remove('hide');
@@ -95,26 +97,27 @@ function switchContent() {
 };
 
 function showLibrary() {
-    savedFilms.forEach( function (object, index) {
+    savedFilms.forEach(function (object, index) {
         filmListItem = document.createElement('div')
         filmListItem.classList.add('filmItem')
-        filmListItem.id = `film-${index +1}`
+        filmListItem.id = `film-${index + 1}`
         filmPoster = document.createElement('img')
         filmPoster.src = `https://image.tmdb.org/t/p/w300/${object.poster_path}`
         filmTitle = document.createElement('p')
         filmTitle.textContent = object.title
         deleteButton = document.createElement('button')
         deleteButton.innerHTML = 'Remove From List'
-        deleteButton.addEventListener('click', function (e) 
-        {savedFilms.splice(index, 1);
-        removeAllChildNodes(filmContainer);
-        showLibrary(); })
+        deleteButton.addEventListener('click', function (e) {
+            savedFilms.splice(index, 1);
+            removeAllChildNodes(filmContainer);
+            showLibrary();
+        })
         filmListItem.appendChild(filmTitle)
         filmListItem.appendChild(filmPoster)
         filmListItem.appendChild(deleteButton)
         filmContainer.appendChild(filmListItem)
     }
-        )
+    )
 
 }
 
@@ -125,14 +128,14 @@ function removeAllChildNodes(parent) {
 }
 
 
-newFilmButton.addEventListener('click' , function (e) { getMovie(latestFilm); });
+newFilmButton.addEventListener('click', function (e) { getMovie(latestFilm); });
 
-saveFilmButton.addEventListener('click' , function (e) {if (savedFilms.includes(currentFilmData) === false) savedFilms.unshift(currentFilmData)} );
+saveFilmButton.addEventListener('click', function (e) { if (savedFilms.includes(currentFilmData) === false) savedFilms.unshift(currentFilmData) });
 
-exportButton.addEventListener('click' , function (e) { downloadCSV()});
+exportButton.addEventListener('click', function (e) { downloadCSV() });
 
-toggleButton1.addEventListener('click', switchContent);   
+toggleButton1.addEventListener('click', switchContent);
 
-toggleButton2.addEventListener('click', switchContent);     
+toggleButton2.addEventListener('click', switchContent);
 
 
